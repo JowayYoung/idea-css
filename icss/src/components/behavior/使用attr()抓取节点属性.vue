06bb806ee761238e3c653e7e30f9f7aa9d2578bf
JowayYoung <young.joway@aliyun.com>
2020-08-24 19:28:16 +0800

    更新

--- icss/src/components/behavior/使用attr()抓取节点属性.vue
+++ icss/src/components/behavior/使用attr()抓取节点属性.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="bruce flex-ct-y" data-title="使用attr()抓取节点属性">
-		<a class="hover-tips" href="https://www.baidu.com" data-msg="Hello World">提示框</a>
-		<a class="hover-tips" href="https://www.baidu.com"></a>
+		<a class="hover-tips btn-1" href="https://www.baidu.com" data-msg="Hello World">提示框</a>
+		<a class="hover-tips btn-2" href="https://www.baidu.com"></a>
 	</div>
 </template>
 
@@ -14,32 +14,31 @@
 	background-color: #66f;
 	line-height: 40px;
 	color: #fff;
-	&::after {
-		position: absolute;
-		left: 0;
-		top: 0;
-		border-radius: 5px;
-		width: 100%;
-		height: 100%;
-		background-color: rgba(#000, .5);
-		opacity: 0;
-		text-align: center;
-		font-size: 12px;
-		content: attr(data-msg);
-		transition: all 300ms;
-	}
-	&:hover::after {
-		left: calc(100% + 20px);
-		opacity: 1;
-	}
-	&[href]:empty::before {
-		content: attr(href);
-	}
-	&[href]:empty:hover::after {
-		display: none;
-	}
 	& + .hover-tips {
 		margin-top: 10px;
+	}
+	&.btn-1 {
+		&::after {
+			position: absolute;
+			left: 0;
+			top: 0;
+			border-radius: 5px;
+			width: 100%;
+			height: 100%;
+			background-color: rgba(#000, .5);
+			opacity: 0;
+			text-align: center;
+			font-size: 12px;
+			content: attr(data-msg);
+			transition: all 300ms;
+		}
+		&:hover::after {
+			left: calc(100% + 20px);
+			opacity: 1;
+		}
+	}
+	&.btn-2:empty::after {
+		content: attr(href);
 	}
 }
 </style>
