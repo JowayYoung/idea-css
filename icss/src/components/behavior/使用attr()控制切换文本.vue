92631f02ba5042bdee75f80699734dc2f2f25459
JowayYoung <young.joway@aliyun.com>
2020-10-03 20:15:53 +0800

    更新

--- icss/src/components/behavior/使用attr()控制切换文本.vue
+++ icss/src/components/behavior/使用attr()控制切换文本.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="bruce flex-ct-x" data-title="使用attr()控制切换文本">
 		<input id="text-switch" class="text-switch" type="checkbox" hidden>
-		<label for="text-switch" data-true="打开" data-false="关闭"></label>
+		<label for="text-switch" data-true="展开" data-false="收起"></label>
 	</div>
 </template>
 
